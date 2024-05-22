9dea1c2b2d4ca376259ade5bdeead52d288c2290
Unai Perez <49728036+Unaiperezdiaz@users.noreply.github.com>
2024-05-22 14:45:56 +0200

    Update discover.py

--- discover.py
+++ discover.py
@@ -1,25 +1,26 @@
 import os
 
-def discover():
-    home = os.environ['HOME']
-    carpetas = [x for x in os.listdir(home) if not x.startswith('.')]
-    extensiones = ['.mp3', '.mp4', '.rar', '.avi', '.png', '.jpeg', '.zip', '.dat', '.txt', '.pdf', '.exe', '.msi',
-                    'iv', '.pem', '.tar']
-    
+def discover(extensiones):
+    # Usar os.scandir en lugar de os.listdir para un mejor rendimiento
     with open('file_list', 'w+') as file_list:
-        for carpeta in carpetas:
-            ruta = os.path.join(home, carpeta)
-            for extension in extensiones:
-                for rutabs, directorio, archivo in os.walk(ruta):
-                    for file in archivo:
-                        if file.endswith(extension):
-                            file_list.write(os.path.join(rutabs, file)+'\n')
+        for entrada in os.scandir(os.environ['HOME']):
+            if entrada.is_dir() and not entrada.name.startswith('.'):
+                for rutabs, _, archivos in os.walk(entrada.path):
+                    # Comprobar la extensión del archivo en lugar de si el archivo termina con la extensión
+                    for archivo in archivos:
+                        if os.path.splitext(archivo)[1] in extensiones:
+                            file_list.write(os.path.join(rutabs, archivo)+'\n')
 
 def main():
-    discover()
+    # Solicitar al usuario que introduzca las extensiones de archivo que desea buscar
+    extensiones_usuario = input("Por favor, introduce las extensiones de archivo que deseas buscar, separadas por comas: ")
+    # Convertir la cadena de entrada del usuario en un conjunto de extensiones
+    extensiones = set(extension.strip() for extension in extensiones_usuario.split(','))
+    discover(extensiones)
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        print("Interrupción del teclado recibida, saliendo...")
         exit()

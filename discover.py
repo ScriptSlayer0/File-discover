4be00b455607c11caadb87513c2cd5b273652a37
Unai Perez <unaipdd@gmail.com>
2024-06-25 17:49:09 +0200

    Option to search path (FIXED)

--- discover.py
+++ discover.py
@@ -2,14 +2,13 @@ import os, platform
 
 def find_home()->str:
     if "Windows" in platform.platform():
-        return "HOMEPATH"
-    return "HOME"
+        return os.environ["USERPROFILE"]
+    return os.environ["HOME"]
 
 def discover(extensions, search_path):
-    for entry in os.scandir(os.environ[find_home()]):
+    for entry in os.scandir(find_home()):
         if entry.is_dir() and not entry.name.startswith('.'):
             for rutabs, _, files in os.walk(entry.path):
-                # Comprobar la extensión del archivo en lugar de si el archivo termina con la extensión
                 for file in files:
                     if os.path.splitext(file)[1] in extensions:
                         print(os.path.join(rutabs, file))
@@ -19,23 +18,20 @@ def discover(extensions, search_path):
                 for entry in os.scandir(path):
                     if entry.is_dir() and not entry.name.startswith('.'):
                         for rutabs, _, files in os.walk(entry.path):
-                            # Comprobar la extensión del archivo en lugar de si el archivo termina con la extensión
                             for file in files:
                                 if os.path.splitext(file)[1] in extensions:
                                     print(os.path.join(rutabs, file))
             except: pass
-
 
 def check_affirm(affirmation):
     if affirmation.lower()=="y" or affirmation.lower()=="yes":
         return True
     else:
         return False
+
 def main():
-    # Solicitar al usuario que introduzca las extensiones de archivo que desea buscar
     user_extensions = input("Please enter the file extensions you want to search for, separated by commas: ")
     search_path = check_affirm(input("Would you like to search path too [y/n]: "))
-    # Convertir la cadena de entrada del usuario en un conjunto de extensiones
     extensions = set(extension.strip() for extension in user_extensions.split(','))
     discover(extensions, search_path)
 

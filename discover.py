bb7eee4a4e0510c438409a0507711e536240291f
Unai Perez <unaipdd@gmail.com>
2024-08-03 13:34:26 +0200

    Discovering beyond disk C: in windows
    psutil integrating

--- discover.py
+++ discover.py
@@ -1,35 +1,50 @@
-import os, platform
+import os, platform, psutil
 
-def find_home()->str:
+def screen_cleaner():
+    if os.name == 'nt': #windows
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def find_home() -> str:
     if "Windows" in platform.platform():
         return os.environ["USERPROFILE"]
     return os.environ["HOME"]
 
+def detect_disks() -> list:
+    partitions = psutil.disk_partitions()
+    disks = [partition.mountpoint for partition in partitions if not partition.mountpoint.startswith('/sys')]
+    return disks
+
 def discover(extensions, search_path):
-    for entry in os.scandir(find_home()):
+    home_directory = find_home()
+    
+    # Search in the user's home directory
+    for entry in os.scandir(home_directory):
         if entry.is_dir() and not entry.name.startswith('.'):
-            for rutabs, _, files in os.walk(entry.path):
+            for root, _, files in os.walk(entry.path):
                 for file in files:
                     if os.path.splitext(file)[1] in extensions:
-                        print(os.path.join(rutabs, file))
+                        print(os.path.join(root, file))
+    
+    # Search in other disks
     if search_path:
-        for path in os.environ['PATH'].split(os.pathsep):
-            try:
-                for entry in os.scandir(path):
+        disks = detect_disks()
+        for disk in disks:
+            if disk != home_directory:  # Avoid searching in the user's home directory again
+                for entry in os.scandir(disk):
                     if entry.is_dir() and not entry.name.startswith('.'):
-                        for rutabs, _, files in os.walk(entry.path):
+                        for root, _, files in os.walk(entry.path):
                             for file in files:
                                 if os.path.splitext(file)[1] in extensions:
-                                    print(os.path.join(rutabs, file))
-            except: pass
+                                    print(os.path.join(root, file))
 
 def check_affirm(affirmation):
-    if affirmation.lower()=="y" or affirmation.lower()=="yes":
-        return True
-    else:
-        return False
+    return affirmation.lower() in ["y", "yes"]
 
 def main():
+    screen_cleaner()
+    print("Welcome to file discover")
     user_extensions = input("Please enter the file extensions you want to search for, separated by commas: ")
     search_path = check_affirm(input("Would you like to search path too [y/n]: "))
     extensions = set(extension.strip() for extension in user_extensions.split(','))
@@ -39,5 +54,8 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("\n"+"Keyboard interruption received, exiting...")
+        print("\nKeyboard interruption received, exiting...")
+        exit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
         exit()

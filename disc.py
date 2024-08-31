85d500e67a5c37edbab70f30ff4c6a2d576916c8
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-08-30 20:42:34 -0600

    add command arg version (./disc.py -p ext)

---
+++ disc.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import argparse,discover
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Store all inputs in a single list.")
+
+# Add a list argument
+parser.add_argument("ext", nargs="*", help="The extenstions you want to search for, serperated by a space")
+
+# Add a flag that appends a value to the list
+parser.add_argument("-path", "--path", action="store_true", help="search all directories in path too?")
+
+# Parse the arguments
+args = parser.parse_args()
+
+discover.discover(args.ext, args.path)

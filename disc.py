e4fb0d87159e1136d268812d2d6ed929440701f2
ScriptSlayer0 <49728036+ScriptSlayer0@users.noreply.github.com>
2024-09-01 03:49:58 +0200

    keyboard interrupt

--- disc.py
+++ disc.py
@@ -1,17 +1,19 @@
 #!/bin/python3
 
 import argparse,discover
+try:
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Store all inputs in a single list.")
 
-# Create the parser
-parser = argparse.ArgumentParser(description="Store all inputs in a single list.")
+    # Add a list argument
+    parser.add_argument("ext", nargs="*", help="The extenstions you want to search for, serperated by a space")
 
-# Add a list argument
-parser.add_argument("ext", nargs="*", help="The extenstions you want to search for, serperated by a space")
+    # Add a flag that appends a value to the list
+    parser.add_argument("-p", "--path", action="store_true", help="search all directories in path too?")
 
-# Add a flag that appends a value to the list
-parser.add_argument("-p", "--path", action="store_true", help="search all directories in path too?")
+    # Parse the arguments
+    args = parser.parse_args()
 
-# Parse the arguments
-args = parser.parse_args()
-
-discover.discover(args.ext, args.path)
+    discover.discover(args.ext, args.path)
+except KeyboardInterrupt:
+    print("KeyboardInterrupt detected")
